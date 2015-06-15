8375ede9706909d3557eb7a686d51802d5bc970c
Jason Ball <jasonlukeball@me.com>
2015-06-15 17:25:43 +1000

    Ruby Object Types

--- 02_Ruby Object Types/02_02_variables.rb
+++ 02_Ruby Object Types/02_02_variables.rb
@@ -5,30 +5,43 @@
 # irb
 x + 2
 # NameError: undefined local variable or method `x' for main:Object from (irb):1
+
 x = 1
 # => 1
+
 x + 2
 # => 3
+
 puts x + 2
 # 3
 # => nil
+
 first_variable = 3
 # => 3
+
 aw_counter = 100
 # => 100
+
 articles_written = 100
 # => 100
+
 var = 1
 # => 1
+
 a = 100
 # => 100
+
 b = a
 # => 100
+
 b
 # => 100
+
 a = 50
 # => 50
+
 b
 # => 100
+
 quit
 
---
+++ notes.rb
@@ -0,0 +1,186 @@
+# Single line comment
+
+=begin
+
+  Multi line comments are wrapped in =begin & =end tags
+
+=end
+
+# Objects are instances of classes
+
+
+# --------------------------
+# --------------------------
+# OBJECT TYPES
+# --------------------------
+# --------------------------
+
+
+# --------------------------
+# VARIABLES
+# --------------------------
+
+# Variables are not not objects
+# Variables are named as all lowercase with underscores eg my_variable_name
+
+
+# Variable scope indicators
+# --------------------------
+
+# Lets ruby know the scope of the variable, where it should be available
+
+# Global      $variable
+# Class       @@variable
+# Instance    @variable
+# Local       variable
+# Block       variable
+
+
+# --------------------------
+# INTEGERS
+# --------------------------
+
+# Fixnum = int
+# BigNum = bitint
+# Float = decimal
+
+#.class
+# Tells you what class the object has
+
+# .abs
+# Returns the absolute number
+
+# .next
+# Adds 1 to a whole number
+
+# .to_i
+# Turn a float into an int
+
+# .to_f
+# Turn into into a float
+
+# .floor
+# round down to a whole number
+
+# .ceil
+# round up to a whole number
+
+
+# --------------------------
+# STRINGS
+# --------------------------
+
+# \t
+# Tab
+
+# \n
+# Line break
+
+# greeting = "Hello"
+# "Say #{Greeting}"
+# => Say Hello
+
+# puts "1 + 1 = #{ 1 + 1 }"
+
+"hello world".capitalize
+# Capitalise first word
+
+"hello world".upcase
+# Uppercase
+
+"HELLO WORLD".downcase
+# lowercase
+
+"HELLO WORLD".reverse
+# Reverse
+
+"HELLO WORLD".length
+# Length
+
+"HELLO WORLD".reverse.downcase
+# Reverse & Lowercase
+
+
+# --------------------------
+# ARRAYS
+# --------------------------
+
+# my_array = ["a", "b", "c"]
+# New Array
+
+# my_array[0]
+# Get first value
+
+# my_array[0] = "A"
+# Replace first value
+
+# my_array << "d"
+# Append to array
+
+# my_array.clear
+# Empty Array
+
+# my_array = nil
+# Delete Array
+
+# my_array = []
+# New empty array
+
+
+
+# Array Methods
+# --------------------------
+
+# my_array.inspect
+# inspect the contents of the array
+
+# my_array.to_s
+# Convert array to string
+
+# my_array.join
+# Merge all values in array into a string
+# my_array.join(", ")
+# Merge all values in array into a string (separate with comma and space)
+
+# x = "1,2,3,4,5"
+# => "1,2,3,4,5"
+# x.split(",")
+# => ["1", "2", "3", "4", "5"]
+# Makes x (string) into an array
+
+# my_array.sort
+# Sorts array
+
+# my_array.reverse
+# Reverse sort
+
+# my_array.uniq
+# Generates a new array with only unique value from my_array
+
+# my_array.uniq!
+# Gets unique values from my_array and updates my_array to this new unique set
+
+# my_array.delete_at(2)
+# Deletes the value at position 2
+
+# my_array.delete("myValue")
+# Delete value by name
+
+# my_array.push("something")
+# Another method to append an array
+
+# my_array.pop
+# Delete the last item from the array
+
+# my_array.shift("myValue")
+# Take the value out of the array
+
+# my_array.unshift
+# Put the value back into the array
+
+
+# my_array1 + my_array2
+# Add two arrays together
+
+
+
