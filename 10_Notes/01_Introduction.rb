176fc5f43c02f51092ba28ac7cbf22239ca15690
Jason Ball <jasonlukeball@me.com>
2015-06-29 21:26:18 +1000

    Notes refactor

--- 10_Notes/01_Introduction.rb
+++ 10_Notes/01_Introduction.rb
@@ -13,6 +13,42 @@
 
 =end
 
+
+
+# Objects
 # Objects are instances of classes
+
+=begin
+
+--------------
+ OBJECTS
+--------------
+
+OBJECTS are things
+(eg a Person)
+
+OBJECTS have attributes
+(eg name, age, gender)
+
+
+--------------
+ METHODS
+--------------
+
+METHODS are things an OBJECT can do
+(eg a person can walk, drive, code)
+
+METHODS are procedures associated with a CLASS
+
+
+--------------
+ CLASSES
+--------------
+
+CLASSES are blueprints of OBJECTS
+(you need to have a class before you can create an object)
+
+
+=end
 
 
--- 10_Notes/05_Methods.rb
+++ 10_Notes/05_Methods.rb
@@ -3,3 +3,22 @@
 # METHODS
 # --------------------------
 # --------------------------
+
+=begin
+A method (or message) in object-oriented programming (OOP) is a procedure associated with an object class.
+An object is made up of behavior and data. Data is represented as properties of the object and behavior as methods.
+Methods are also the interface an object presents to the outside world.
+=end
+
+def hello_ruby
+  puts "Hello Ruby!"
+end
+
+hello_ruby
+# Calls the method
+
+# Returns....
+# Hello Ruby!
+# => nil
+
+
