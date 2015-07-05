f335fe4c92f11360167767f24b72f2a88c8b7d79
Jason Ball <jasonlukeball@me.com>
2015-07-05 22:10:28 +1000

    Classes: Object Atributes

--- 06_Classes/06_03_ruby_sandbox/classes.rb
+++ 06_Classes/06_03_ruby_sandbox/classes.rb
@@ -1,11 +1,51 @@
+
+
 class Animal
-  def make_noise
-    "Moo!"
+
+  def set_noise(the_noise)
+    return @noise = the_noise
   end
+
+
+  def make_noise
+    return @noise
+  end
+
 end
 
-animal1 = Animal.new
-puts animal1.make_noise
+line_break = "------------------------------"
+puts line_break
 
-animal2 = Animal.new
-puts animal2.make_noise
+
+cow = Animal.new
+puts "cow"
+puts cow
+cow.set_noise("Moo!")
+puts cow.make_noise
+
+
+puts line_break
+
+
+chicken = Animal.new
+puts "chicken"
+puts chicken
+chicken.set_noise("Cluck!")
+puts chicken.make_noise
+
+
+puts line_break
+
+
+dog = Animal.new
+puts "dog"
+puts dog
+dog.set_noise("Woof!")
+puts dog.make_noise
+
+
+
+puts line_break
+
+
+
