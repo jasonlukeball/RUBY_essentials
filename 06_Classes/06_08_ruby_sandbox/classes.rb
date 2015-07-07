e2f0a6aa6e7996ec1a654efb43a06a8292623f69
Jason Ball <jasonlukeball@me.com>
2015-07-07 14:35:22 +1000

    Classes: Class Attributes

--- 06_Classes/06_08_ruby_sandbox/classes.rb
+++ 06_Classes/06_08_ruby_sandbox/classes.rb
@@ -1,48 +1,79 @@
-class Animal
-  attr_accessor :name
-  attr_writer :color
-  attr_reader :legs, :arms
-  
+class FarmAnimal2
+
+
+  # ------------------
+  # ATTRIBUTE METHODS
+  # ------------------
+  attr_accessor :noise
+  attr_accessor :color
+
+  # ------------------
+  # CLASS VARIABLE
+  # ------------------
+  @@all_species = ['cat','cow','dog','duck','horse','pig',]
+  # Track total animals created
+  @@total_animals = 0
+  # Track the FarmAnimal2 instances that have been created
+  @@current_animals = []
+
+  # ------------------
+  # CLASS METHODS
+  # ------------------
+
   def self.all_species
-    ['cat', 'cow', 'dog', 'duck', 'horse', 'pig']
+    @@all_species
   end
-  
+
   def self.create_with_attributes(noise, color)
-    animal = self.new(noise)
-    animal.color = color
-    return animal
+    animal = FarmAnimal2.new(noise,color)
+    # Because we call .new it now runs the 'initialize' method
+    return animal # This is not really needed
   end
-  
-  def initialize(noise, legs=4, arms=0)
+
+  def self.current_animals
+    @@current_animals
+  end
+
+
+
+  # ------------------
+  # INSTANCE METHODS
+  # ------------------
+  def initialize(noise,color)
     @noise = noise
-    @legs = legs
-    @arms = arms
-    puts "A new animal has been instantiated."
+    @color = color
+    # New animal created, increase the @@total_animals class variable
+    @@total_animals += 1
+    # New animal created, append it to the @@current_animals class variable
+    @@current_animals << self
+
+
+    puts '-----------------------------------'
+    puts 'A new animal has been instantiated.'
+    puts '-----------------------------------'
+    puts @noise
+    puts @color
+    puts @@total_animals
   end
-  
-  def noise=(noise)
-    @noise = noise
-  end
-  
-  def noise
-    @noise
-  end
-  
-  def color
-    "The color is #{@color}."
-  end
+
+
 end
 
-puts Animal.all_species.inspect
 
-animal1 = Animal.new("Moo!", 4, 0)
-animal1.name = "Steve"
-puts animal1.name
-animal1.color = "black"
-puts animal1.color
-puts animal1.legs
-puts animal1.noise
+# Call the 'all species method' (which gets it value from the class variable '@@species')
+puts FarmAnimal2.all_species.inspect
 
-animal2 = Animal.create_with_attributes("Quack!", "white")
-puts animal2.noise
-puts animal2.color
+# Call the Class Method 'create_with_attributes'
+# Because this calls .new it also runs the 'initialise' Method
+FarmAnimal2.create_with_attributes('Woof','Black')
+
+# Create another FarmAnimal2 instance
+# Notice the @@total_animals will be '2'
+FarmAnimal2.create_with_attributes('Baah','White')
+
+puts "\n"
+puts '------------------------------------'
+puts 'Current Animals (@@current_animals)'
+puts '------------------------------------'
+puts FarmAnimal2.current_animals.inspect
+puts "\n"
--- 10_Notes/06_Classes.rb
+++ 10_Notes/06_Classes.rb
@@ -378,14 +378,98 @@ FarmAnimal.create_with_attributes('Woof','Black')
 # CLASS ATTRIBUTES
 # -----------------------------
 
+# Class attributes are used to store values that apple to the class generally
+# Class attributes are stored in class variables ( @@variable )
+
+
+class FarmAnimal2
+
+
+  # ------------------
+  # ATTRIBUTE METHODS
+  # ------------------
+  attr_accessor :noise
+  attr_accessor :color
+
+  # ------------------
+  # CLASS VARIABLE
+  # ------------------
+  @@all_species = ['cat','cow','dog','duck','horse','pig',]
+  # Track total animals created
+  @@total_animals = 0
+  # Track the FarmAnimal2 instances that have been created
+  @@current_animals = []
+
+  # ------------------
+  # CLASS METHODS
+  # ------------------
+
+  def self.all_species
+    @@all_species
+  end
+
+  def self.create_with_attributes(noise, color)
+    animal = FarmAnimal2.new(noise,color)
+    # Because we call .new it now runs the 'initialize' method
+    return animal # This is not really needed
+  end
+
+  def self.current_animals
+    @@current_animals
+  end
 
 
 
+  # ------------------
+  # INSTANCE METHODS
+  # ------------------
+  def initialize(noise,color)
+    @noise = noise
+    @color = color
+    # New animal created, increase the @@total_animals class variable
+    @@total_animals += 1
+    # New animal created, append it to the @@current_animals class variable
+    @@current_animals << self
+
+
+    puts '-----------------------------------'
+    puts 'A new animal has been instantiated.'
+    puts '-----------------------------------'
+    puts @noise
+    puts @color
+    puts @@total_animals
+  end
+
+
+end
+
+
+# Call the 'all species method' (which gets it value from the class variable '@@species')
+puts FarmAnimal2.all_species.inspect
+
+# Call the Class Method 'create_with_attributes'
+# Because this calls .new it also runs the 'initialise' Method
+FarmAnimal2.create_with_attributes('Woof','Black')
+
+# Create another FarmAnimal2 instance
+# Notice the @@total_animals will be '2'
+FarmAnimal2.create_with_attributes('Baah','White')
+
+puts "\n"
+puts '------------------------------------'
+puts 'Current Animals (@@current_animals)'
+puts '------------------------------------'
+puts FarmAnimal2.current_animals.inspect
+puts "\n"
 
 
 # -----------------------------
 # CLASS READER/WRITER METHODS
 # -----------------------------
+
+
+
+
 
 # -----------------------------
 # INHERITANCE
