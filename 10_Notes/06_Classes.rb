7a2ac0477fbd4164c67a77184a2ba8b9ecd075c8
Jason Ball <jasonlukeball@me.com>
2015-07-07 14:07:40 +1000

    Classes: Class Methods

--- 10_Notes/06_Classes.rb
+++ 10_Notes/06_Classes.rb
@@ -321,12 +321,67 @@ def self.method_name
 end
 
 
+class FarmAnimal
 
+
+  # ------------------
+  # ATTRIBUTE METHODS
+  # ------------------
+  attr_accessor :noise
+  attr_accessor :color
+
+  # ------------------
+  # CLASS METHODS
+  # ------------------
+  def self.all_species
+    ['cat','cow','dog','duck','horse','pig',]
+  end
+
+  def self.create_with_attributes(noise, color)
+    animal = FarmAnimal.new(noise,color)
+    # Because we call .new it now runs the 'initialize' method
+    return animal # This is not really needed
+  end
+
+
+
+  # ------------------
+  # INSTANCE METHODS
+  # ------------------
+  def initialize(noise,color)
+    @noise = noise
+    @color = color
+
+    puts '-----------------------------------'
+    puts 'A new animal has been instantiated.'
+    puts '-----------------------------------'
+    puts @noise
+    puts @color
+  end
+
+
+end
+
+
+# Call the Class Method 'all_species'
+puts FarmAnimal.all_species.inspect
+
+# Create new instance using only the 'initialize' instance method
+# duck = FarmAnimal.new('Quack!', 'Brown')
+
+# Call the Class Method 'create_with_attributes'
+# Because this calls .new it also runs the 'initialise' Method
+FarmAnimal.create_with_attributes('Woof','Black')
 
 
 # -----------------------------
 # CLASS ATTRIBUTES
 # -----------------------------
+
+
+
+
+
 
 # -----------------------------
 # CLASS READER/WRITER METHODS
