60518069201fd4e195ce7adec1a2e396d05e5d2a
Jason Ball <jasonlukeball@me.com>
2015-07-11 13:58:48 +1000

    Modules: Namespacing

--- 10_Notes/07_Modules.rb
+++ 10_Notes/07_Modules.rb
@@ -3,3 +3,25 @@
 # MODULES
 # --------------------------
 # --------------------------
+
+# Modules are wrappers around Ruby code
+# Modules are used in conjunction with classes
+
+# Reasons to use Modules:
+
+# Namespaces
+# --------------------------
+# Namespaces allow you to have class names which don't conflict
+# For example if we had a class names 'Date' and used Date.new, this would conflict with Ruby's default Date class which creates a date
+
+# Wrapping the class Date inside of a Module, keeps things completely separate from any other classes
+module DateOfMeeting
+  class Date
+    # ...
+  end
+end
+
+# Create a new object from the Date class within the DateOfMeeting Module
+meeting = DateOfMeeting::Date.new
+
+
